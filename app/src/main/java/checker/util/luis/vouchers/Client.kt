16aa2e11a2816b620186a0980040f47e62986813
Luis Tinajero Muñozcano <ltm150895@gmail.com>
2018-02-13 16:26:40 -0600

    trying things on the client

--- app/src/main/java/checker/util/luis/vouchers/Client.kt
+++ app/src/main/java/checker/util/luis/vouchers/Client.kt
@@ -1,14 +1,14 @@
 package checker.util.luis.vouchers
 
+import android.util.Log
 import checker.util.luis.vouchers.model.Balance
 import checker.util.luis.vouchers.utils.exception.BadRequestException
 import checker.util.luis.vouchers.utils.exception.InternalErrorException
 import checker.util.luis.vouchers.utils.exception.NotFoundException
 import checker.util.luis.vouchers.utils.exception.UnauthorizedException
 import checker.util.luis.vouchers.utils.gson
-import okhttp3.FormBody
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import okhttp3.*
+import java.io.IOException
 
 
 object VoucherClient {
@@ -29,11 +29,14 @@ object VoucherClient {
             .add("TARJETA" , cardNumber)
             .build()
 
-        return Request.Builder()
+        val result = Request.Builder()
             .url(url)
             .post(form)
             .build()
-            .execute()
+
+        result.executeAsync<Balance>()
+
+        return  result.tryExecute() ?: Balance("","")
     }
 }
 
@@ -44,11 +47,67 @@ private inline fun <reified T> Request.execute() : T {
     val stream = when (code) {
         200 -> response.body()!!.charStream() // TODO, check the assert not null
         400 -> throw BadRequestException("Url : $url")
-        401 -> {
-            throw UnauthorizedException("Url : $url")
-        }
+        401 -> throw UnauthorizedException("Url : $url")
         404 -> throw NotFoundException(url)
         else -> throw InternalErrorException("Unexpected response code $code for $url")
     }
     return gson.fromJson(stream, T::class.java).apply { stream.close() }
+}
+
+private inline fun <reified T> Request.executeAsync() {
+    VoucherClient.VOUCHER_CLIENT.newCall(this)
+        .enqueue(
+                object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        Log.d("Some error my friend", e.message)
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        val url = url().toString()
+                        val code = response.code()
+                        val stream = when(code) {
+                            200 -> response.body()!!.charStream() // TODO , check assert not null
+                            400 -> throw  BadRequestException("Url : $url")
+                            401 -> throw  UnauthorizedException("Url : $url")
+                            404 -> throw NotFoundException(url)
+                            else -> throw InternalErrorException("Unexpected response code $code for $url")
+                        }
+
+                        val result = gson.fromJson(stream, T::class.java).apply { stream.close() }
+
+//                        when(result::class) { // why this is invalid ?
+//                            Int::class -> ""
+//                            Balance::class -> ""
+//                        }
+
+                        when(T::class) {
+                            Int::class -> ""
+                            Balance:: class -> ""
+                        }
+                    }
+                }
+        )
+    TODO()
+}
+
+private inline fun <reified T> Request.tryExecute() : T? {
+        try {
+            val response = VoucherClient.VOUCHER_CLIENT.newCall(this)
+                .execute()
+            val url = url().toString()
+            val code = response.code()
+            val stream = when (code) {
+                200 -> response.body()!!.charStream() // TODO, check the assert not null
+                400 -> throw BadRequestException("Url : $url")
+                401 -> {
+                    throw UnauthorizedException("Url : $url")
+                }
+                404 -> throw NotFoundException(url)
+                else -> throw InternalErrorException("Unexpected response code $code for $url")
+            }
+            return gson.fromJson(stream, T::class.java).apply { stream.close() }
+        } catch (e : Exception){
+            Log.d("Some error my friend", e.message)
+            return null
+        }
 }
